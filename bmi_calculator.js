48a5c2c2213280458202176cb25edc120f3f00cb
TedRicafranca <ted314089@gmail.com>
2022-11-12 09:52:49 +0800

    Manual Testing for BMI Calculator.

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,16 +1,47 @@
 function calculateBMI(height, weight){
     let bmi = (weight / ((height * height) / 10000)).toFixed(2);
     let resultBMI = "";
-    if (bmi < 18.6){
-        resultBMI = `Under Weight : ${bmi}`;
-    }else if (bmi >= 18.6 && bmi < 24.9){
+    if (bmi < 16){
+        resultBMI = `Severe Under Weight : ${bmi}`;
+    }else if (bmi > 16 && bmi <= 17){
+        resultBMI = `Moderate Under Weight : ${bmi}`;
+    }else if (bmi > 17 && bmi <= 18.5){
+        resultBMI = `Mild Under Weight : ${bmi}`;
+    }else if (bmi > 18.5 && bmi <= 25){
         resultBMI = `Normal : ${bmi}`;
-    }else{
+    }else if (bmi > 25 && bmi <= 30){
         resultBMI = `Over Weight : ${bmi}`;
+    }else if (bmi > 30 && bmi <= 35){
+        resultBMI = `Obese Class I : ${bmi}`;
+    }else if (bmi > 35 && bmi <= 40){
+        resultBMI = `Obese Class II : ${bmi}`;
+    }else{
+        resultBMI = `Obese Class III : ${bmi}`;
     }
     return resultBMI;   
 }
 
 //Height in inches
 //Weight in kg
-console.log(calculateBMI(180,65));
+console.log(calculateBMI(180,40)); //Correct
+console.log(calculateBMI(180,55));  //Correct
+console.log(calculateBMI(180,58));  //Correct
+console.log(calculateBMI(180,60));  //Correct
+console.log(calculateBMI(180,85));  //Correct
+console.log(calculateBMI(180,100));  //Correct
+console.log(calculateBMI(180,120));  //Correct
+console.log(calculateBMI(180,150));  //Correct
+
+
+//weight / ((height * height) / 10000)  BMI Formula
+// 40/((180 * 180) / 10000) = 12.35     bmi < 16   Severe Under Weight
+// 55/((180 * 180) / 10000) = 16.98     bmi > 16 && bmi <= 17   Moderate Under Weight
+// 58/((180 * 180) / 10000) = 17.90     bmi > 17 && bmi <= 18.5   Mild Under Weight
+// 60/((180 * 180) / 10000) = 18.52     bmi > 18.5 && bmi <= 25  Normal
+// 85/((180 * 180) / 10000) = 26.23     bmi > 25 && bmi <= 30   Over Weight
+// 100/((180 * 180) / 10000) = 30.86    bmi > 30 && bmi <= 35   Obese Class I
+// 120/((180 * 180) / 10000) = 37.04    bmi > 30 && bmi <= 35   Obese Class II
+// 150/((180 * 180) / 10000) = 46.30    bmi > 40                Obese Class III
+
+let bmitest = (150 / ((180 * 180) / 10000)).toFixed(2);
+console.log(bmitest);
